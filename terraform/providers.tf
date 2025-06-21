868a7bc4ba279f76666cd1ecea46fcc1ca5e7828
uday singh <uday.singh@dhcs.ca.gov>
2025-06-21 02:46:01 -0400

    Added specific version for required providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,33 +2,57 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.70" # Pin to a specific 5.x version
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.13" # or latest tested version
+      version = "~> 2.13"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.27"
     }
+    null = {
+      source = "hashicorp/null"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
+  required_version = ">= 1.0"
 }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 5.0"
+#     }
+#     helm = {
+#       source  = "hashicorp/helm"
+#       version = "~> 2.13" # or latest tested version
+#     }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "~> 2.27"
+#     }
+#   }
+# }
+
 
 provider "aws" {
   region = var.region
 }
-
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_ca)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks.cluster_ca)
-#     token                  = data.aws_eks_cluster_auth.cluster.token
-#   }
-# }
